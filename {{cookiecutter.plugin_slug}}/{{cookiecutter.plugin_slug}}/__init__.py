2dd410c7c8104321131470273a1d8f6579e4d439
Chris Kotfila <chris.kotfila@kitware.com>
2017-11-27 14:47:09 -0500

    Stub out basic python package info

---
+++ {{cookiecutter.plugin_slug}}/{{cookiecutter.plugin_slug}}/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for {{ cookiecutter.plugin_name }}."""
+
+__author__ = """{{ cookiecutter.author_name}}"""
+__email__ = '{{ cookiecutter.email }}'
+__version__ = '{{ cookiecutter.version }}'
